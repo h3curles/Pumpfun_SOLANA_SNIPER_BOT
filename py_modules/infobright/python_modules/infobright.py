f26c20d7a376c0d16d88132c26a723f3823d9202
h3curles <3254543+h3curles@users.noreply.github.com>
2024-12-07 10:35:15 -0800

    Update infobright.py

--- py_modules/infobright/python_modules/infobright.py
+++ py_modules/infobright/python_modules/infobright.py
@@ -2,7 +2,7 @@
 The MIT License
 
 Copyright (c) 2008 Gilad Raphaelli <gilad@raphaelli.com>
-
+ 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
