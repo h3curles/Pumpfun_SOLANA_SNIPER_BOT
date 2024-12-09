090e1b53b8096de5c29e6c19dbbb9c584295e901
h3curles <3254543+h3curles@users.noreply.github.com>
2024-12-09 09:17:18 -0800

    Update api_chain.py

--- api_chain.py
+++ api_chain.py
@@ -2,7 +2,7 @@
 import pytest  
 from solders.keypair import Keypair 
 from solders.message import Message        
-from solders.transaction_status import ParsedInstruction 
+from solders.transaction_status import ParsedInstruction  
 from spl.memo.constants import MEMO_PROGRAM_ID  
 from spl.memo.instructions import MemoParams, create_memo   
    
