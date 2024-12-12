8ca892ad13f0e95eae7a693ed4d6d07b7a24a483
h3curles <3254543+h3curles@users.noreply.github.com>
2024-12-12 08:32:53 -0800

    Update test_http_client.py

--- init/test_http_client.py
+++ init/test_http_client.py
@@ -3,7 +3,7 @@ from typing import Tuple
 
 import pytest
 import solders.system_program as sp
-from solders.keypair import Keypair
+from solders.keypair import Keypair 
 from solders.message import MessageV0, Message
 from solders.pubkey import Pubkey
 from solders.rpc.errors import SendTransactionPreflightFailureMessage
